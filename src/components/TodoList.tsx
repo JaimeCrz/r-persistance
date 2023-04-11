1ab008d7216835540cb10c577dcc97efca54134e
jaime <jaime.cruz.hostalot@gmail.com>
2023-04-11 18:32:28 +0200

    scaffolding done

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,16 +1,65 @@
 import React, { useState } from "react";
 
-export const TodoList = () => {
-    return (
-        <div className="main">
-            <h1>Todo List</h1>
-            <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-            </ul>
-            <input type="text" placeholder="Add todo Item"/>
-            <button>Add Item</button>
-        </div>
-    )
+interface item {
+  id: number;
+  text: string;
+  completed: boolean;
 }
+
+export const TodoList = () => {
+  const [todos, setTodos] = useState<item[]>([
+    {
+      id: 1,
+      text: "Learn React",
+      completed: false,
+    },
+    {
+      id: 2,
+      text: "Learn Redux",
+      completed: false,
+    },
+    {
+      id: 3,
+      text: "Learn Node",
+      completed: false,
+    },
+    {
+      id: 4,
+      text: "Learn Express",
+      completed: false,
+    },
+  ]);
+
+  const [inputText, setInputText] = useState("");]
+
+  const handleToggle = (id: number) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      })
+    );
+  };
+
+  const handleClick = () => {
+    const newTodos: item = {id: Date.now(), text: inputText, completed: false};
+    setTodos([...todos, newTodos]);
+  };
+
+  return (
+    <div className="main">
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id} onClick={() => handleToggle(todo.id)}>
+            {todo.text}
+          </li>
+        ))}
+      </ul>
+      <input type="text" placeholder="Add todo Item" onChange={(e)=> setInputText(e.currentTarget.value)} />
+      <button onClick={}>Add Item</button>
+    </div>
+  );
+};
