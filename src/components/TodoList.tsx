cd3217138fc99e13e817aa328dbe1004164f39ed
jaime <jaime.cruz.hostalot@gmail.com>
2023-04-11 16:42:03 +0200

    scaffold app

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -4,6 +4,13 @@ export const TodoList = () => {
     return (
         <div className="main">
             <h1>Todo List</h1>
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+            </ul>
+            <input type="text" placeholder="Add todo Item"/>
+            <button>Add Item</button>
         </div>
     )
 }
