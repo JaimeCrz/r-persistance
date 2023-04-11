2f6eb53bbbc59fee33b358e33b4043da19997512
jaime <jaime.cruz.hostalot@gmail.com>
2023-04-11 16:09:27 +0200

    SetUp R

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
 import './styles/App.css';
+import { TodoList } from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
+      <TodoList />
     </div>
   );
 }
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+export const TodoList = () => {
+    return (
+        <div className="main">
+            <h1>Todo List</h1>
+        </div>
+    )
+}
